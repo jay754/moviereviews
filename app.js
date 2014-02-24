f6168ee207af4d3e2f45c46d3b8227d2ab85d76c
Jay Engineer <jayengineer6@gmail.com>
2014-02-23 21:47:29 -0500

    got the request to work

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
+/**
+
+U mad b?
+App.js
+
+**/
+
 //module intialization for express
 var express = require("express");
 var reddit = require("redwrap");
+var request = require("request");
 var analyze = require('Sentimental').analyze //analyze sentimental value for a sentence
 
 //function for finding out the score for the sentence
 var score = function(sentence){
-	return analyze(sentence)["score"];
+  return analyze(sentence)["score"];
 }
 
+var redditData = function(callback, username){
+	
+	request({
+	  uri: 'http://www.reddit.com/user/' + username +'/comments.json', 
+	  method: "GET",
+	  timeout: 10000,
+	  followRedirect: true,
+	  maxRedirects: 10
+	}, function(error, response, body){
+	     //callback(body);
+	     getData(JSON.parse(body));
+	});
+}
+
+var getData = function(data){
+  console.log(data);
+}
 //console.log(score("I love you")); //Score: -6, Comparative:-1.5
-
-//reddit wrapper test
-reddit.r('awww').sort('top').all(function(res){
-  res.on('data', function(data, res) {
-        console.log(data); //a parsed javascript object of the requested data
-         //the raw response data from Reddit
-    });
-
-    res.on('error', function(e) {
-        console.log(e); //outputs any errors
-    });
-
-    res.on('end', function(){
-        console.log('All Done');
-    }); //outputs a parsed javascript object represeting
-});
