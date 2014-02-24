b162abbc79f19d29455288bba0031812c9fe2e57
Jay Engineer <jayengineer6@gmail.com>
2014-02-23 19:35:16 -0500

    reddit test

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 //module intialization for express
 var express = require("express");
+var reddit = require("redwrap");
 
 //analyze sentimental value for a sentence
 var analyze = require('Sentimental').analyze,
     positivity = require('Sentimental').positivity,
     negativity = require('Sentimental').negativity;
 
-console.log(analyze("fucking asshole")["score"]); //Score: -6, Comparative:-1.5
+console.log(analyze("I love you")["score"]); //Score: -6, Comparative:-1.5
+
+//reddit wrapper test
+reddit.r('awww').sort('top').all(function(res){
+  res.on('data', function(data, res) {
+        console.log(data); //a parsed javascript object of the requested data
+         //the raw response data from Reddit
+    });
+
+    res.on('error', function(e) {
+        console.log(e); //outputs any errors
+    });
+
+    res.on('end', function(){
+        console.log('All Done');
+    }); //outputs a parsed javascript object represeting
+});
