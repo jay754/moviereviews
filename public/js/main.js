4ce33538fcf14915dff8cabdda13a348aaa89488
Jay Engineer <jayengineer6@gmail.com>
2014-03-22 23:41:53 -0400

    got basic ajax call to work

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
 $(function(){
-  console.log('hello world');
+ 
+  $("#b").click(function(){
+    
+    var movie = $("#movie").val();
+    console.log("movie");
+
+    $.ajax({
+
+       url: "/movies/"+movie,
+       type: "GET",
+       // data: {movie_data: movie},
+       dataType: "json",
+
+       success: function(data){
+         console.log(data); //this should start at 1
+       },
+
+       error: function(xhr, ajaxOptions, thrownError){
+         console.log(xhr.status);
+         console.log(thrownError);
+       }
+
+    });
+
+  });
+
 })
